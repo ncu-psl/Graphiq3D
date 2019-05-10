1529167d3150fc3a7cdd4d4d4415c906e8d3fc6e
byjiyi <45142992+byjiyi@users.noreply.github.com>
2019-05-10 09:24:20 +0800

    Delete gridspec.py

--- src/gridspec.py
+++
@@ -1,8 +0,0 @@
-from parameter import *
-
-x0 = y0 = z0 = h = dq = df = x00 = y00 = \
-nxc = nyc = nzc = nx = ny = nz = None
-
-igridx = [] #list length = nxcm1
-igridy = [] #list length = nycm1
-igridz = [] #list length = nzcm1
